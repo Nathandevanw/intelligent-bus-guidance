2db8d81351ff69d8c1928c2636ba76c99c69ab3c
BlueTheCrafter <ponharini@outlook.com>
2026-05-25 13:57:11 +1000

    new Changes

--- src/test/java/com/busguidance/BusIntegrationTest.java
+++ src/test/java/com/busguidance/BusIntegrationTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BusIntegrationTest {
     @Test
+    //This is a test!
     void test() {
         Bus bus = new Bus("12345678",70, 100, "Electricity");
         BusRepository busRepository = new BusRepository();
