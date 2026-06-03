7c37b1279e5d8030f5084be7dcce6a2b13e5f399
Meryem681 <s4165719@student.rmit.edu.au>
2026-06-03 16:17:08 +1000

    bus test fully functional

--- src/main/java/com/busguidance/Bus.java
+++ src/main/java/com/busguidance/Bus.java
@@ -16,6 +16,9 @@ public class Bus {
         this.capacity = capacity;
         this.fuelLevel = fuelLevel;
         this.fuelType = fuelType;
+        this.age = 30;
+        this.expYears = 5;
+        this.license = "PublicTransport";
     }
 
     public String getBusID(){
@@ -26,9 +29,26 @@ public class Bus {
         return capacity;
     }
 
+    public double getFuelLevel() {
+        return fuelLevel;
+    }
+
     public String getFuelType(){
         return fuelType;
     }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public int getExpYears() {
+        return expYears;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
 
     public boolean ValidBusID(){
         return busID != null && busID.matches("\\d{8}");
@@ -55,71 +75,11 @@ public class Bus {
         
             return licenceType.equalsIgnoreCase("Heavy")
             || licenceType.equalsIgnoreCase("PublicTransport");
+            }
+        return true;
     }
-    return true;
+   
 }
-        this.age = 30;
-        this.expYears = 5;
-        this.license = "PublicTransport";
-    }
 
-    // Define the getter functions for the Bus class.
-    public String getBusID(){
-        return this.busID;
-    }
-
-    public int getCapacity() {
-        return this.capacity;
-    }
-
-    public double getFuelLevel() {
-        return this.fuelLevel;
-    }
-    
-    public String getFuelType() {
-        return this.fuelType;
-    }
-
-    public int getAge() {
-        return this.age;
-    }
-
-    public int getExpYears() {
-        return this.expYears;
-    }
-
-    public String getLicense() {
-        return this.license;
-    }
-
-    // Define the setter functions for the Bus Class
-    public void setBusID(String busID) {
-        this.busID = busID;
-    }
-
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
-
-    public void setFuelLevel(double fuelLevel) {
-        this.fuelLevel = fuelLevel;
-    }
-
-    public void setFuelType(String fuelType) {
-        this.fuelType = fuelType;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public void setExpYears(int expYears) {
-        this.expYears = expYears;
-    }
-
-    public void setLicense(String license) {
-        this.license = license;
-    }
-
-}
+        
 
